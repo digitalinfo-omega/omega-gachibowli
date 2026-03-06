0849b2c95a1580fe2b4757eef30d3b9c3993faf2
Nithish <nithishsunny1234@gmail.com>
2026-03-06 15:46:52 +0530

    added aos

--- components/layout/HeroSection.tsx
+++ components/layout/HeroSection.tsx
@@ -15,27 +15,51 @@ export default function HeroSection() {
 
       <div className="relative z-10 h-full flex items-center justify-center md:justify-start px-4">
         <div className="max-w-7xl mx-auto w-full">
-          <div className="max-w-2xl text-center md:text-left mx-auto md:mx-0">
-            <h1 className="text-[26px] md:text-[40px] font-semibold text-secondary mb-4">
+          <div className="max-w-2xl text-center md:text-left mx-auto md:mx-0 md:mb-25">
+            <h1
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              className="text-[26px] md:text-[40px] font-semibold text-secondary mb-4"
+            >
               World-Class Cancer Care
             </h1>
 
-            <h2 className="font-bold mb-4">
+            <h2
+              className="font-bold mb-4"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="100"
+            >
               <span className="text-gray-400 heading">Right Here in </span>
               <span className="text-accent text-[26px] md:text-[40px]">
                 Hyderabad
               </span>
             </h2>
 
-            <p className="text-gray-600 mb-8 xl-para md:max-w-[70%] mx-auto md:mx-0">
+            <p
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="200"
+              className="text-gray-600 mb-8 xl-para md:max-w-[70%] mx-auto md:mx-0"
+            >
               Comprehensive cancer treatment and multi-specialty care with
               India’s leading oncologists.
             </p>
 
             {/* Stats */}
-            <div className="backdrop-blur-md bg-white/70 rounded-xl px-6 md:px-11 py-5 flex flex-col md:flex-row gap-6 md:gap-10 w-fit mx-auto md:mx-0 items-center">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="300"
+              className="backdrop-blur-md bg-white/70 rounded-xl px-6 md:px-11 py-5 flex flex-col md:flex-row gap-6 md:gap-10 w-fit mx-auto md:mx-0 items-center"
+            >
               {/* Stat 1 */}
-              <div className="flex items-center gap-4">
+              <div
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="400"
+                className="flex items-center gap-4"
+              >
                 <h3 className="heading font-bold text-secondary tracking-tight">
                   52L+
                 </h3>
@@ -48,10 +72,20 @@ export default function HeroSection() {
               </div>
 
               {/* Divider */}
-              <div className="hidden md:block h-16 w-px bg-gray-300"></div>
+              <div
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="500"
+                className="hidden md:block h-16 w-px bg-gray-300"
+              ></div>
 
               {/* Stat 2 */}
-              <div className="flex items-center gap-4">
+              <div
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay="600"
+                className="flex items-center gap-4"
+              >
                 <h3 className="heading font-bold text-accent tracking-tight">
                   66k+
                 </h3>
